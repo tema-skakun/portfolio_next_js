1ee1de43dd1d9439195c0cb974513ff15f55b316
tema-skakun <frederico_f@mail.ru>
2024-07-27 14:55:05 +0300

    fix: publishing to gh action

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/portfolio_next_js",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-    ],
-  },
-  reactStrictMode: true,
   output: "export",
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
